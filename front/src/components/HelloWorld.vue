3f9d365e4fbcc87493f1d9d949ccf8c549f9305f
UnderdogMario <61845086+UnderdogMario@users.noreply.github.com>
2020-06-17 14:14:53 -0700

    figure out the router

---
+++ front/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Hello Word Vue</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HelloWorld"
+    }
+</script>
+
+<style scoped>
+
+</style>
